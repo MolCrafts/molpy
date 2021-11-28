50e8ec0a091e04115c198bae41da8e4c69f015b2
kylin <kylincaster@gmail.com>
2021-11-28 18:57:26 +0800

    fix the LF again

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 from copy import deepcopy
 import ase
 import molpy as mp
+
+
 class A:
-    
     def __init__(self) -> None:
-        self.data = {'key': 'value'}
-        self.stable = 'stable'
+        self.data = {"key": "value"}
+        self.stable = "stable"
         self.myself = self
-        
+
+
 a = A()
 aa = deepcopy(a)
 
@@ -18,7 +20,9 @@ angles = ase_cell.angles()
 lengths = ase_cell.lengths()
 
 lbox = lengths[0]
-box = mp.Box("ppp", lx = lbox, ly = lbox, lz = lbox, alpha = angles[0], beta = angles[1], gamma = angles[2])
+box = mp.Box(
+    "ppp", lx=lbox, ly=lbox, lz=lbox, alpha=angles[0], beta=angles[1], gamma=angles[2]
+)
 
 basis = box._basis.T
-box2 = mp.Box("ppp", a1 = basis[0], a2 = basis[1], a3 = basis[2])
+box2 = mp.Box("ppp", a1=basis[0], a2=basis[1], a3=basis[2])
