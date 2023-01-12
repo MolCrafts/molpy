012a367fbf87e87fe3a1363b02fb371e801137f2
Roy-Kid <lijichen365@126.com>
2023-01-12 19:21:49 +0800

    replace graph_cpp vector with map

--- cpp/graph.h
+++ cpp/graph.h
@@ -5,15 +5,14 @@
 #include <queue>
 #include <iostream>
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 namespace py = pybind11;
 
 namespace molpy
 {
-
     template <class dataType> // Type of idx vertex will hold
     class Graph
     {
-        int numOfVertices; // number of vertices.
         struct Vertex;     // forward declaration of vertex structure
 
         struct Node
@@ -32,30 +31,32 @@ namespace molpy
         {
             visitedState state; // state of vertex, visited/being visited/done
             dataType label;      // the label of vertex 
+            size_t idx;            // the index of vertex
             Node *list;         // Pointer to all edges (linkedlist)
-            size_t idx;     // index of the vertex
         };
 
-        std::vector<Vertex> vertices; // vector of all vertices.
+        std::map<size_t, Vertex> vertices; // map of all vertices.
 
         // private methods
         Node *getNode(Vertex *);                   // allocate and initialize a newnode for the adj list.
         void insertAtEnd(Node *&, Vertex *);       // insert at the end of adjacency list of vertex.
         void deleteAllAfter(Node *);               // delete the adjacency list of the vertex.
         void depth_first_traversal_util(Vertex *); // Private utility function for DFS
+        Vertex create_vertex(size_t, dataType);    // Create a vertex
+        Vertex get_vertex(size_t);
 
     public:
-        Graph();                             // Default constructor
-        Graph(std::vector<dataType> &);      // Constructor which takes vector of vertex idx
-        Graph(py::array_t<dataType, py::array::c_style | py::array::forcecast> &labels); // Constructor which takes numpy array of vertex idx
+        Graph(py::array_t<dataType, py::array::c_style | py::array::forcecast> &);
+        Graph() = default;                             // Default constructor
         void set_edge(size_t, size_t);   // For setting a edge of graph
         bool has_edge(size_t, size_t);   // For checking if a edge exists
-        void append_vertex(dataType);
+        void set_vertex(size_t, dataType);
+        bool has_vertex(size_t);
         void display() const;                // Print current config of the graph.
         int get_num_of_vertices() const;     // Get number of vertices in the graph
-        void breadth_first_search(dataType); // Breadth first traversal of the graph
-        void depth_first_search(dataType);   // Depth first traversal of the 
-        Vertex create_vertex(dataType);    // Create a vertex
+        dataType get_vertex_label(size_t);   // Get the label of the vertex
+        void breadth_first_search(size_t); // Breadth first traversal of the graph
+        void depth_first_search(size_t);   // Depth first traversal of the 
         ~Graph();
     }; // end of class Graph
 
@@ -67,6 +68,32 @@ namespace molpy
         newNode->vertexPtr = v;
         newNode->next = nullptr;
         return newNode;
+    }
+
+    template <typename dataType>
+    dataType Graph<dataType>::get_vertex_label(size_t idx)
+    {
+        return get_vertex(idx).label;
+    }
+
+    template <typename dataType>
+    bool Graph<dataType>::has_vertex(size_t idx)
+    {
+        return vertices.find(idx) != vertices.end();
+    }
+
+    template <typename dataType>
+    typename Graph<dataType>::Vertex Graph<dataType>::get_vertex(size_t idx)
+    {
+        auto it = vertices.find(idx);
+        if (it != vertices.end())
+        {
+            return it->second;
+        }
+        else
+        {
+            throw "Vertex not found";
+        }
     }
 
     template <typename dataType>
@@ -101,132 +128,68 @@ namespace molpy
     }
 
     template <typename dataType>
-    Graph<dataType>::Graph() // Default constructor
-        : numOfVertices(0),
-          vertices(0)
-    {
-    }
-
-    template <typename dataType>
-    Graph<dataType>::Graph(std::vector<dataType> &labels) // takes a vector of vertices labels
-        : numOfVertices(labels.size()),
-          vertices(numOfVertices)
-    {
-        for (size_t i = 0; i < numOfVertices; ++i)
-        {
-            vertices[i].idx = labels[i];
-            vertices[i].list = nullptr;
-            vertices[i].state = WHITE;
-            vertices[i].idx = i;
-        }
-    }
-
-    template <typename dataType>
-    Graph<dataType>::Graph(py::array_t<dataType, py::array::c_style | py::array::forcecast> &labels) // takes a numpy array of vertices labels
-        : numOfVertices(labels.size()),
-          vertices(numOfVertices)
-    {
-        for (size_t i = 0; i < numOfVertices; ++i)
-        {
-            vertices[i].idx = labels.at(i);
-            vertices[i].list = nullptr;
-            vertices[i].state = WHITE;
-            vertices[i].idx = i;
-        }
-    }
-
-    template <typename dataType>
     void Graph<dataType>::set_edge(size_t idx1, size_t idx2) // Setting individual edge of the graph.
     {
 
-        if (idx1 > numOfVertices - 1 || idx2 > numOfVertices - 1)
-        {
-            size_t vertices_to_add = idx1 > idx2 ? idx1 : idx2;
-            for (size_t i = numOfVertices; i <= vertices_to_add; ++i)
-            {
-                dataType none_label = std::numeric_limits<dataType>::max();
-                append_vertex(none_label);
-            }
-        }
+        if (!has_vertex(idx1)) set_vertex(idx1, std::numeric_limits<dataType>::max());
+        if (!has_vertex(idx2)) set_vertex(idx2, std::numeric_limits<dataType>::max());
 
-        for (int i = 0; i < numOfVertices; ++i)
-        {
-            if (vertices[i].idx == idx1)
-            {
-                for (int j = 0; j < numOfVertices; ++j)
-                {
-                    if (vertices[j].idx == idx2)
-                    {
-                        insertAtEnd(vertices[i].list, &vertices[j]);
-                        break;
-                    }
-                }
-                break;
-            }
-        }
+        insertAtEnd(vertices[idx1].list, &vertices[idx2]);
+
     }
 
     template <typename dataType>
     bool Graph<dataType>::has_edge(size_t idx1, size_t idx2) // Setting individual edge of the graph.
     {
-        for (int i = 0; i < numOfVertices; ++i)
+
+        if (!has_vertex(idx1) || !has_vertex(idx2)) return false;
+
+        Node *temp = vertices[idx1].list;
+        while (temp != nullptr)
         {
-            if (vertices[i].idx == idx1)
+            if (temp->vertexPtr->idx == idx2)
             {
-                for (int j = 0; j < numOfVertices; ++j)
-                {
-                    if (vertices[j].idx == idx2)
-                    {
-                        Node *temp = vertices[i].list;
-                        while (temp != nullptr)
-                        {
-                            if (temp->vertexPtr->idx == idx2)
-                            {
-                                return true;
-                            }
-                            temp = temp->next;
-                        }
-                        return false;
-                    }
-                }
-                break;
+                return true;
             }
+            temp = temp->next;
         }
         return false;
+
     }
 
 
     template <typename dataType>
-    void Graph<dataType>::append_vertex(dataType label)  // Append empty vertex to the graph
+    void Graph<dataType>::set_vertex(size_t idx, dataType label)  // Append empty vertex to the graph
     {
-        vertices.push_back(create_vertex(label));
-        ++numOfVertices;
+        if (!has_vertex(idx))
+        {
+            vertices[idx] = create_vertex(idx, label);
+        }
     }
 
     template <typename dataType>
-    Vertex Graph<dataType>::create_vertex(dataType label)
+    typename Graph<dataType>::Vertex Graph<dataType>::create_vertex(size_t idx, dataType label)
     {
         Vertex v;
         v.label = label;
         v.list = nullptr;
         v.state = WHITE;
-        v.index = numOfVertices;
+        v.idx = idx;
         return v;
     }
 
     template <typename dataType>
     void Graph<dataType>::display() const // Prints the current config of the graph
     {
-        Node *node;
-        for (int i = 0; i < numOfVertices; ++i)
+        std::cout << "Graph: " << std::endl;
+        for (auto it = vertices.begin(); it != vertices.end(); ++it)
         {
-            std::cout << "Vertex:" << vertices[i].idx << " ";
-            std::cout << "Connections: ";
-            node = vertices[i].list;
-            while (node != nullptr)
+            std::cout << it->first << " : " << it->second.label << " : ";
+            Node *temp = it->second.list;
+            while (temp != nullptr)
             {
-                std::cout << node->vertexPtr->idx << " ";
-                node = node->next;
+                std::cout << temp->vertexPtr->idx << " ";
+                temp = temp->next;
             }
             std::cout << std::endl;
         }
@@ -235,106 +198,138 @@ namespace molpy
     template <typename dataType>
     int Graph<dataType>::get_num_of_vertices() const // Returns the number of vertices in the graph
     {
-        return numOfVertices;
+        return vertices.size();
     }
 
     template <typename dataType>
-    void Graph<dataType>::breadth_first_search(dataType startElem) // Breadth first traversal of the graph
+    void Graph<dataType>::breadth_first_search(size_t start) // Breadth first traversal of the graph
     {
+        if (!has_vertex(start))
+        {
+            throw "Vertex not found";
+        }
         // mark all vertices as not visited, i.e. state = WHITE
-        for (int i = 0; i < numOfVertices; ++i)
+        for (auto it = vertices.begin(); it != vertices.end(); ++it)
         {
-            vertices[i].state = WHITE;
+            it->second.state = WHITE;
         }
-
-        // search for the vertex containing start element
-        Vertex *startVertex = nullptr;
-        for (int i = 0; i < numOfVertices; ++i)
+ 
+        std::queue<Vertex *> q;
+        q.push(&vertices[start]);
+        vertices[start].state = GRAY;
+        while (!q.empty())
         {
-            if (vertices[i].idx == startElem)
+            Vertex *v = q.front();
+            q.pop();
+            std::cout << v->label << " ";
+            Node *temp = v->list;
+            while (temp != nullptr)
             {
-                startVertex = &vertices[i];
-                break;
-            }
-        }
-
-        // Return if start vertex not found
-        if (startVertex == nullptr)
-        {
-            return;
-        }
-
-        // Create a queue for traversing breadth wise.
-        std::queue<Vertex *> vertexQueue;
-
-        // mark the first vertex as being processed
-        startVertex->state = GRAY;
-        // push the first vertex
-        vertexQueue.push(startVertex);
-        Vertex *currVertex = nullptr;
-
-        while (!vertexQueue.empty())
-        {
-            currVertex = vertexQueue.front();
-            vertexQueue.pop();
-            currVertex->state = BLACK;
-            std::cout << currVertex->idx << " " << std::endl;
-            Node *adjVertex = currVertex->list;
-            while (adjVertex != nullptr)
-            {
-                if (adjVertex->vertexPtr->state == WHITE)
+                if (temp->vertexPtr->state == WHITE)
                 {
-                    adjVertex->vertexPtr->state = GRAY;
-                    vertexQueue.push(adjVertex->vertexPtr);
+                    temp->vertexPtr->state = GRAY;
+                    q.push(temp->vertexPtr);
                 }
-                adjVertex = adjVertex->next;
+                temp = temp->next;
             }
+            v->state = BLACK;
         }
         std::cout << std::endl;
+
+
+        // search for the vertex containing start element
+        // Vertex *startVertex = nullptr;
+        // for (int i = 0; i < numOfVertices; ++i)
+        // {
+        //     if (vertices[i].idx == startElem)
+        //     {
+        //         startVertex = &vertices[i];
+        //         break;
+        //     }
+        // }
+
+        // // Return if start vertex not found
+        // if (startVertex == nullptr)
+        // {
+        //     return;
+        // }
+
+        // // Create a queue for traversing breadth wise.
+        // std::queue<Vertex *> vertexQueue;
+
+        // // mark the first vertex as being processed
+        // startVertex->state = GRAY;
+        // // push the first vertex
+        // vertexQueue.push(startVertex);
+        // Vertex *currVertex = nullptr;
+
+        // while (!vertexQueue.empty())
+        // {
+        //     currVertex = vertexQueue.front();
+        //     vertexQueue.pop();
+        //     currVertex->state = BLACK;
+        //     std::cout << currVertex->idx << " " << std::endl;
+        //     Node *adjVertex = currVertex->list;
+        //     while (adjVertex != nullptr)
+        //     {
+        //         if (adjVertex->vertexPtr->state == WHITE)
+        //         {
+        //             adjVertex->vertexPtr->state = GRAY;
+        //             vertexQueue.push(adjVertex->vertexPtr);
+        //         }
+        //         adjVertex = adjVertex->next;
+        //     }
+        // }
+        // std::cout << std::endl;
     }
 
     template <typename dataType>
     void Graph<dataType>::depth_first_traversal_util(Vertex *v) // Depth first search private utility function
     {
-        v->state = GRAY;
-        std::cout << v->idx << " ";
-        Node *node = v->list;
-        while (node != nullptr)
+        if (v == nullptr)
         {
-            if (node->vertexPtr->state == WHITE)
+            return;
+        }
+        v->state = GRAY;
+        std::cout << v->label << " ";
+        Node *temp = v->list;
+        while (temp != nullptr)
+        {
+            if (temp->vertexPtr->state == WHITE)
             {
-                depth_first_traversal_util(node->vertexPtr);
+                depth_first_traversal_util(temp->vertexPtr);
             }
-            node = node->next;
+            temp = temp->next;
         }
         v->state = BLACK;
     }
 
     template <typename dataType>
-    void Graph<dataType>::depth_first_search(dataType startElem) // Public function for depth first traversal
+    void Graph<dataType>::depth_first_search(size_t idx) // Public function for depth first traversal
     {
-        for (int i = 0; i < numOfVertices; ++i)
+
+        if (!has_vertex(idx))
         {
-            vertices[i].state = WHITE;
+            throw "Vertex not found";
         }
-        for (int i = 0; i < numOfVertices; ++i)
+        // mark all vertices as not visited, i.e. state = WHITE
+        for (auto it = vertices.begin(); it != vertices.end(); ++it)
         {
-            if (vertices[i].idx == startElem)
-            {
-                depth_first_traversal_util(&vertices[i]);
-                break;
-            }
+            it->second.state = WHITE;
         }
+        depth_first_traversal_util(&vertices[idx]);
         std::cout << std::endl;
+
     }
 
     template <typename dataType>
     Graph<dataType>::~Graph()
     {
-        for (int i = 0; i < numOfVertices; ++i)
+        for (auto it = vertices.begin(); it != vertices.end(); ++it)
         {
-            deleteAllAfter(vertices[i].list);
+            deleteAllAfter(it->second.list);
         }
+        
     }
 } // end of namespace molpy
 
--- cpp/tests/test_graph.cpp
+++ cpp/tests/test_graph.cpp
@@ -1,7 +1,24 @@
 #include <gtest/gtest.h>
 #include "graph.h"
 
-TEST(GraphTest, GraphTest) {
+TEST(TestGraph, TestAppendVertex) {
+    molpy::Graph<int> graph;
+    graph.set_vertex(1, 0);
+    ASSERT_EQ(graph.get_num_of_vertices(), 1);
+    ASSERT_EQ(graph.get_vertex_label(1), 0);
+}
+
+TEST(TestGraph, TestSetEdge) {
+    molpy::Graph<int> graph;
+    graph.set_edge(0, 1);
+    ASSERT_EQ(graph.get_num_of_vertices(), 2);
+    graph.set_edge(0, 2);
+    ASSERT_EQ(graph.get_num_of_vertices(), 3);
+    graph.set_edge(1, 2);
+    ASSERT_EQ(graph.get_num_of_vertices(), 3);
+}
+
+TEST(TestGraph, TestHasEdge) {
     molpy::Graph<int> graph;
     graph.set_edge(0, 1);
     graph.set_edge(0, 2);
