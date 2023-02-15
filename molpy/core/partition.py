a9bd3e6cd4629c4a2315864bd65919b155778ae5
Roy-Kid <lijichen365@126.com>
2023-02-15 11:52:07 +0800

    rewrite cell and nblist, cell has passed tests

--- molpy/core/partition.py
+++ molpy/core/partition.py
@@ -8,75 +8,77 @@ from .box import Box
 import numpy as np
 from itertools import product
 
+def calc_cell_index_by_coord(cell_coords, cells_per_side):
+    """
+    Calculate the cell index by the cell coordinates.
+
+    Parameters
+    ----------
+    cell_coords : (N, 3)
+        cell coordinates
+    cells_per_side : (3, )
+        # of cell in each direction
+
+    Returns
+    -------
+    (N, )
+        cell index
+    """
+    return cell_coords[:, 0] + cell_coords[:, 1] * cells_per_side[0] + cell_coords[:, 2] * cells_per_side[0] * cells_per_side[1]
 
 class CellList:
 
-    def __init__(self, index_list, cells_per_side):
+    def __init__(self, xyz, xyz_cell_coord, cells_per_side, cell_lattice):
         self.cells_per_side = cells_per_side
-        self.index_list = index_list
+        self.xyz = xyz
+        self.xyz_cell_coord = xyz_cell_coord
+        self.cell_lattice = cell_lattice
+        self.xyz_cell_index = calc_cell_index_by_coord(xyz_cell_coord, cells_per_side)
 
-    def get_all_cell_indices(self, only_occupied=True):
-        """Get the indices of all cells."""
-        if only_occupied:
-            return self.index_list
-        else:
-            return np.array([i for i in product(range(self.cells_per_side[0]), range(self.cells_per_side[1]), range(self.cells_per_side[2]))])
+    def get_xyz_by_cell_coords(self, coords):
+        """
+        Get the xyz of the atoms in the given cell coordinates.
 
-    def get_shell_cell_indices(self, index):
-        """Get the cells that are within one shell of the cell containing the given atom."""
-        # Compute the cell index for the given atom.
-        dx = np.array([i for i in product([0, -1], repeat=3)])
+        Parameters
+        ----------
+        coords : (m, n, 3)
+            The cell coordinates of the cell, where m is # of coords set, n is # of coords in each set, 3 is dimension. 
 
-        shell_cell_indices = index + dx
-
-        shell_cell_indices = np.where(shell_cell_indices < 0, shell_cell_indices + self.cells_per_side, shell_cell_indices)
-
-        return shell_cell_indices
-
-    def get_indices(self, cell_indices):
-
-        # Compute the cell index for the given atom.
-        cell_index = cell_indices[:, 0] + cell_indices[:, 1] * self.cells_per_side[0] + cell_indices[:, 2] * self.cells_per_side[0] * self.cells_per_side[1]
-
-        _cell_index = self.index_list[:, 0] + self.index_list[:, 1] * self.cells_per_side[0] + self.index_list[:, 2] * self.cells_per_side[0] * self.cells_per_side[1]
-
+        Returns
+        -------
+        (m*n, 3)
+            xyz of the atoms in the given cell coordinates.
+        """
+        xyz_cell_index = self.xyz_cell_index
+        cell_index = np.zeros(coords.shape[:-1], dtype=np.int32)
+        # convert coords to cell index
+        for i, coord in enumerate(coords):
+            cell_index[i] = calc_cell_index_by_coord(coord, self.cells_per_side)
+        
         # Compute the indices of the atoms in the given cell.
-        indices = np.argwhere(np.isin(_cell_index, cell_index))
+        indices = np.isin(xyz_cell_index, cell_index.reshape((-1, 3)))
 
-        return indices.reshape(-1)
+        return self.xyz[indices]
 
 
 def calc_cell_dimensions(box_matrix:np.ndarray, minimum_cell_size):
     """Compute the number of cells-per-side and total number of cells in a box."""
 
-    # Compute the number of cells per side.
-    cell_length = box_matrix.diagonal() / minimum_cell_size
-    cells_per_side = np.floor(cell_length).astype(np.int32)
-    # xy, xz, yz = box_matrix[0, 1], box_matrix[0, 2], box_matrix[1, 2]
-    # cell_lattice = np.array(
-    #     [cell_length[0], xy / cells_per_side[0], xz / cells_per_side[0], 0, cell_length[1], yz / cells_per_side[1], 0, 0, cell_length[2]]
-    # ).reshape((3, 3))
+    box_length = box_matrix.diagonal()
+    cells_per_side = np.floor(box_length / minimum_cell_size)
     cell_lattice = box_matrix / cells_per_side
 
     return cell_lattice, cells_per_side
 
-def create_cellList(box, xyz, rCutoff):
-
-    if isinstance(box, np.ndarray):
-        box = Box.from_matrix(box)
-
-    # Compute the number of cells per side.
-    # Assume box is a standard parallelepiped.
-    cell_lattice, cells_per_side = calc_cell_dimensions(box._matrix, rCutoff)
-
-    # Compute the cell indices for each atom.
-    reci_r = np.dot(xyz, np.linalg.inv(cell_lattice).T)
+def calc_cell_coord(xyz, cell_lattice, cells_per_side):
+    """Compute the cell coordinate for each atom."""
 
     # Compute the cell coordinate for each atom.
-    cell_indices = np.floor(reci_r).astype(int)
+    inv_cell_lattice = np.linalg.inv(cell_lattice)
+    atom_indices = np.floor(np.dot(xyz, inv_cell_lattice.T))
 
     # Compute the cell index for each atom.
-    cell_index = cell_indices[:, 0] + cell_indices[:, 1] * cells_per_side[0] + cell_indices[:, 2] * cells_per_side[0] * cells_per_side[1]
+    # atom_index = atom_indices[:, 0] + atom_indices[:, 1] * cells_per_side[0] + atom_indices[:, 2] * cells_per_side[0] * cells_per_side[1]
 
     # Sort the atoms by cell index.
     # sorted_indices = np.argsort(cell_index)
@@ -84,24 +86,41 @@ def create_cellList(box, xyz, rCutoff):
     # sorted_cell_index = cell_index[sorted_indices]
 
     # Compute the number of atoms in each cell.
-    # cell_counts = np.bincount(sorted_cell_index)
+    # cell_counts = np.bincount(atom_index)
 
     # Compute the index of the first atom in each cell.
     # cell_offsets = np.zeros(len(cell_counts) + 1, dtype=np.int32)
     # cell_offsets[1:] = np.cumsum(cell_counts)
 
-    return CellList(cell_indices, cells_per_side)
+    return atom_indices
+
+def create_cellList(box, xyz, rCutoff):
+
+    # 1. standardize periodic boundary condition
+    # Assume box is a standard parallelepiped.
+    if isinstance(box, np.ndarray):
+        box = Box.from_matrix(box)
+
+    # 2. Compute the size of cells per side.
+    cell_lattice, cells_per_side = calc_cell_dimensions(box._matrix, rCutoff)
+
+    # 3. Compute the cell coordinate for each atom.
+    xyz_cell_coord = calc_cell_coord(xyz, cell_lattice, cells_per_side)
+
+    return CellList(xyz, xyz_cell_coord, cells_per_side, cell_lattice)
 
 class NeighborList:
 
-    def __init__(self, indices):
+    def __init__(self, indices, dr, r):
         self.indices = indices
+        self.dr = dr
+        self.r = r
 
     def get_pairs(self):
         """
         get the indices of atoms that are within the cutoff distance
         """
-        return 
+        
 
     def get_neighbors(self):
         """
@@ -109,35 +128,61 @@ class NeighborList:
         """
         pass
 
+def get_shell_cells(centerCellIndices, cellPerSide, rShell=1, isSymmetricCell:bool=True):
 
-def create_neighborList(box, xyz, rCutoff, isCellList:bool=True):
+    dx = np.array([i for i in product(range(-rShell, rShell+1), repeat=3)])
+    if isSymmetricCell:
+        dx = dx[:int(len(dx) / 2)+1]
+        shell_cell_coord = centerCellIndices + dx
+        shell_cell_coord = np.where(shell_cell_coord < 0, shell_cell_coord + cellPerSide, shell_cell_coord)  # periodic boundary condition
+    else:
+        shell_cell_coord = centerCellIndices + dx
+        shell_cell_coord = np.where(shell_cell_coord >= cellPerSide, shell_cell_coord - cellPerSide, shell_cell_coord)
+        shell_cell_coord = np.where(shell_cell_coord < 0, shell_cell_coord + cellPerSide, shell_cell_coord)
 
-    box = Box.from_matrix(box)
-    
+    return shell_cell_coord
+
+def get_check_cells(cell_per_side, rShell=1, isSymmetricPair:bool=True):
+    """Get the cells that are within one shell of the cell containing the given atom."""
+    centerCellCoord = np.array([i for i in product(range(cell_per_side[0]), range(cell_per_side[1]), range(cell_per_side[2]))])
+
+    shellCells = np.zeros((len(centerCellCoord), (2*rShell+1)**3, 3), dtype=int)
+    for i, center in enumerate(centerCellCoord):
+        shellCells[i] = get_shell_cells(center, cell_per_side, rShell, isSymmetricPair)
+
+    return centerCellCoord, shellCells
+
+
+def create_neighborList(box, xyz, rCutoff, rSkin=0, isCellList:bool=True, isSymmetricPair:bool=True):
+
+    # 1. standardize periodic boundary condition
+    # Assume box is a standard parallelepiped.
+    if isinstance(box, np.ndarray):
+        box = Box.from_matrix(box)
+
+    # 2. pre-compute arguments
+    cutoff = rCutoff + rSkin
+
+    # 3. get the candidate pairs to compute distance
     if isCellList:
         indices = []
         cellList = create_cellList(box, xyz, rCutoff)
-        cell_indices = cellList.get_all_cell_indices()
-        for cell_index in cell_indices:
-            shell_cell_indices = cellList.get_shell_cell_indices(cell_index)
 
-            _indices = cellList.get_indices(shell_cell_indices)
-            part_xyz = xyz[_indices]
-            # Compute the distance between each pair of atoms.
-            dr = box.displacement(part_xyz[:, None, :], part_xyz[None, :, :])
-            r = np.linalg.norm(dr, axis=-1)
-            r_triu = np.triu(r)
-            mask = np.argwhere(np.logical_and(r_triu < rCutoff, r_triu!=0))
-            
-            indices.append(_indices[mask])
+        centerCellIndices, shellCells = get_check_cells(cellList.cells_per_side, rShell=1, isSymmetricPair=isSymmetricPair)
 
-        indices = np.sort(np.concatenate(indices), axis=0)
+        centerCellXyz = cellList.get_xyz_by_cell_coords(centerCellIndices)
+        shellCellXyz = cellList.get_xyz_by_cell_coords(shellCells)
+
+        # calculate distance pair-wisely
+        centerCellXyzMat = np.tile(centerCellXyz[:, None, :], (1, shellCellXyz.shape[1], 1))
+        shellCellXyzMat = np.tile(shellCellXyz, (centerCellXyz.shape[0], 1, 1))
+        dr = box.displacement(centerCellXyzMat, shellCellXyzMat)
 
     else:
         # Compute the distance between each pair of atoms.
         dr = box.displacement(xyz[:, None, :], xyz[None, :, :])
-        r = np.linalg.norm(dr, axis=-1)
-        r_triu = np.triu(r)
-        indices = np.argwhere(np.logical_and(r_triu < rCutoff, r_triu!=0))
+    r = np.linalg.norm(dr, axis=-1)
+    r_triu = np.triu(r)
+    indices = np.argwhere(np.logical_and(r_triu < cutoff, r_triu!=0))
 
-    return NeighborList(indices)
+    return NeighborList(indices, dr, r)
--- tests/test_partition.py
+++ tests/test_partition.py
@@ -9,12 +9,24 @@ import numpy.testing as npt
 
 class TestCellList:
 
+    def test_calc_cell_index_by_coord(self):
+
+        cell_coords = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        cells_per_side = np.array([3, 3, 3])
+        npt.assert_equal(mp.core.partition.calc_cell_index_by_coord(cell_coords, cells_per_side), np.array([0, 1, 3, 9]))
+
     def test_cellList_utils(self):
 
-        cellList = mp.core.partition.CellList(np.array([]), np.array([3,3,3]))
-        npt.assert_equal(cellList.get_shell_cell_indices(np.array([0, 0, 0])), np.array([[0, 0, 0], [0, 0, 2], [0, 2, 0], [0, 2, 2], [2, 0, 0], [2, 0, 2], [2, 2, 0], [2, 2, 2]]))
+        cellList = mp.core.partition.CellList(
+            np.array([[1, 1, 0], [4, 0, 0], [2, 4, 0]]),  # xyz
+            np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]]),  # xyz_cell_coord
+            np.array([3, 3, 3]),   # cells_per_side
+            np.array([[10/3, 1, 1], [0, 10/3, 1], [0, 0, 10/3]])  # cell_lattice
+        )
 
-        npt.assert_equal(cellList.get_shell_cell_indices(np.array([2, 2, 2])), np.array([[2, 2, 2], [2, 2, 1], [2, 1, 2], [2, 1, 1], [1, 2, 2], [1, 2, 1], [1, 1, 2], [1, 1, 1]]))
+        npt.assert_equal(cellList.xyz_cell_index, np.array([0, 1, 3]))
+
+        npt.assert_equal(cellList.get_xyz_by_cell_coords(np.array([[[0, 0, 0], [1, 0, 0], [0, 1, 0]]])), np.array([[1, 1, 0], [4, 0, 0], [2, 4, 0]]))
 
     def test_calc_cell_dimensions(self):
 
@@ -23,7 +35,7 @@ class TestCellList:
         r_cutoff = 3.0
         cell_lattice, cell_per_side = mp.core.partition.calc_cell_dimensions(box, r_cutoff)
 
-        npt.assert_equal(cell_lattice, np.array([[10/3, 1, 1], [0, 10/3, 1], [0, 0, 10/3]]))
+        npt.assert_equal(cell_lattice, box / 3)
         npt.assert_equal(cell_per_side, np.array([3, 3, 3]))
 
         # create othogonal box
@@ -43,7 +55,11 @@ class TestCellList:
         xyz = np.array([[1, 1, 0], [2, 0, 0], [2, 2, 0]])
         cellList = mp.core.partition.create_cellList(box, xyz, r_cutoff)
 
-        assert cellList
+        npt.assert_equal(cellList.xyz, xyz)
+        npt.assert_equal(cellList.xyz_cell_coord, np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0]]))
+        npt.assert_allclose(cellList.cell_lattice, np.array([[10/6, 0, 0], [0, 10/6, 0], [0, 0, 10/6]]))
+        npt.assert_equal(cellList.cells_per_side, np.array([6, 6, 6]))
+        npt.assert_equal(cellList.xyz_cell_index, np.array([0, 1, 7]))
 
         # create parallelpiped box
         box = np.array([[10, 3, 3], [0, 10, 3], [0, 0, 10]])
@@ -51,9 +67,25 @@ class TestCellList:
         xyz = np.array([[1, 1, 0], [3, 4, 0], [4, 1, 0]])
         cellList = mp.core.partition.create_cellList(box, xyz, r_cutoff)
 
-        assert cellList
+        npt.assert_equal(cellList.xyz, xyz)
+        npt.assert_equal(cellList.xyz_cell_coord, np.array([[0, 0, 0], [0, 1, 0], [1, 0, 0]]))
+        npt.assert_allclose(cellList.cell_lattice, np.array([[10/3, 1, 1], [0, 10/3, 1], [0, 0, 10/3]]))
+        npt.assert_equal(cellList.cells_per_side, np.array([3, 3, 3]))
+        npt.assert_equal(cellList.xyz_cell_index, np.array([0, 3, 1]))
 
 class TestNeighborList:
+
+    def test_neighborList_utils(self):
+
+        dr = np.ones((3, 3, 1, 3))
+
+        nblist = mp.core.partition.NeighborList(
+            np.ones([[0, 1, 1], [1, 0, 1], [0, 1, 1]], dtype=int),  # 0-1-2-0
+            dr,  # dr
+            np.ones([[0, 1, 1], [1, 0, 1], [0, 1, 1]], dtype=int),  # dr_cell
+        )
+
+        pass
 
     def test_create_neighborList(self):
 
