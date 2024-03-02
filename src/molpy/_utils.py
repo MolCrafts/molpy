3f37eadea5e9795326074bcb3b8904cb8ee91b5d
Roy Kid <lijichen365@gmail.com>
2024-03-02 13:14:07 +0100

    refactor alias

--- src/molpy/_utils.py
+++ src/molpy/_utils.py
@@ -4,4 +4,6 @@ class Singleton(type):
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+            return cls._instances[cls]
+        else:
+            return cls._instances[cls](*args, **kwargs)
