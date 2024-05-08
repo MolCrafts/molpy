6cdae3823e6b3ed9500ce206604dabae30a82dbb
Roy Kid <lijichen365@gmail.com>
2024-05-08 15:16:36 +0200

    update alias

--- src/molpy/_alias.py
+++ src/molpy/_alias.py
@@ -9,7 +9,7 @@ import numpy as np
 from dataclasses import dataclass
 
 @dataclass
-class _Alias:
+class AliasEntry:
     alias: str
     key: str
     type: type
@@ -22,75 +22,188 @@ class _Alias:
     def __repr__(self) -> str:
         return f"<Alias: {self.alias}>"
 
-class _Aliases(dict):
 
-    _scopes: dict[str, "_Aliases"] = {}
+DEFAULT_ALIASES = {
+    "timestep": AliasEntry("timestep", "_ts", int, "fs", "time step"),
+    "step": AliasEntry("step", "_step", int, None, "simulation step"),
+    "name": AliasEntry("name", "_name", str, None, "atomic name"),
+    "n_atoms": AliasEntry("n_atoms", "_n_atoms", int, None, "number of atoms"),
+    "n_molecules": AliasEntry(
+        "n_molecules", "_n_molecules", int, None, "number of molecules"
+    ),
+    "xyz": AliasEntry("xyz", "_xyz", np.ndarray, "angstrom", "atomic coordinates"),
+    "R": AliasEntry("xyz", "_xyz", np.ndarray, "angstrom", "atomic coordinates"),
+    "cell": AliasEntry("cell", "_cell", np.ndarray, "angstrom", "unit cell"),
+    "energy": AliasEntry("energy", "_energy", float, "meV", "energy"),
+    "forces": AliasEntry("forces", "_forces", np.ndarray, "eV/angstrom", "forces"),
+    "momenta": AliasEntry(
+        "momenta", "_momenta", np.ndarray, "eV*fs/angstrom", "momenta"
+    ),
+    "charge": AliasEntry("charge", "_charge", float, "C", "charge"),
+    "mass": AliasEntry("mass", "_mass", float, None, ""),
+    "stress": AliasEntry("stress", "_stress", np.ndarray, "GPa", "stress"),
+    "idx": AliasEntry("idx", "_idx", int, None, ""),
+    "molid": AliasEntry("mol", "_molid", int, None, "molecule index"),
+    "Z": AliasEntry("Z", "_atomic_numbers", int, None, "nuclear charge"),
+    "atype": AliasEntry("atype", "_atomic_types", int, None, "atomic type"),
+    "vdw_radius": AliasEntry(
+        "vdw_radius", "_vdw_radius", float, "angstrom", "van der Waals radius"
+    ),
+    "idx_m": AliasEntry("idx_m", "_idx_m", int, None, "indices of systems"),
+    "idx_i": AliasEntry("idx_i", "_idx_i", int, None, "indices of center atoms"),
+    "idx_j": AliasEntry("idx_j", "_idx_j", int, None, "indices of neighboring atoms"),
+    "angle_i": AliasEntry(
+        "angle_i", "_angle_i", int, None, "indices of center atoms for angles"
+    ),
+    "angle_j": AliasEntry(
+        "angle_j", "_angle_j", int, None, "indices of neighboring atoms for angles"
+    ),
+    "angle_k": AliasEntry(
+        "angle_k", "_angle_k", int, None, "indices of neighboring atoms for angles"
+    ),
+    "bond_i": AliasEntry(
+        "bond_i", "_bond_i", int, None, "indices of center atoms for bonds"
+    ),
+    "bond_j": AliasEntry(
+        "bond_j", "_bond_j", int, None, "indices of neighboring atoms for bonds"
+    ),
+    "idx_i_lr": AliasEntry(
+        "idx_i_lr", "_idx_i_lr", int, None, "indices of center atoms for # long-range"
+    ),
+    "idx_j_lr": AliasEntry(
+        "idx_j_lr",
+        "_idx_j_lr",
+        int,
+        None,
+        "indices of neighboring atoms for # long-range",
+    ),
+    "offsets": AliasEntry("offsets", "_offsets", int, None, "cell offset vectors"),
+    "Rij": AliasEntry(
+        "Rij",
+        "_Rij",
+        np.ndarray,
+        "angstrom",
+        "vectors pointing from center atoms to neighboring atoms",
+    ),
+    "dist": AliasEntry(
+        "dist",
+        "_dist",
+        np.ndarray,
+        "angstrom",
+        "distances between center atoms and neighboring atoms",
+    ),
+    "pbc": AliasEntry("pbc", "_pbc", np.ndarray, None, "periodic boundary conditions"),
+    "dipole_moment": AliasEntry(
+        "dipole_moment", "_dipole_moment", np.ndarray, "e*bohr", "dipole moment"
+    ),
+    "partial_charges": AliasEntry(
+        "partial_charges", "_partial_charges", np.ndarray, "e", "partial charges"
+    ),
+    "polarizability": AliasEntry(
+        "polarizability", "_polarizability", np.ndarray, "angstrom^3", "polarizability"
+    ),
+    "element": AliasEntry("element", "_element", str, None, "element symbol"),
+}
 
-    def __init__(self, scope_name: str, definition:dict={}) -> None:
-        super().__init__(definition)
-        if scope_name not in self._scopes:
-            self._scopes[scope_name] = self
 
-    def __getattr__(self, alias: str):
+class NameSpace(dict):
 
-        if alias in self:
-            return self[alias].key
-        else:
-            return self._scopes[alias]
-        
-    def __call__(self, scope_name: str, definition: dict={}) -> None:
-        if scope_name not in self._scopes:
-            _Aliases(scope_name, definition)
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __repr__(self) -> str:
+        return f"<NameSpace: {self.name}>"
 
     def set(self, alias: str, key: str, type: type, unit: str, comment: str) -> None:
-        self[alias] = _Alias(alias, key, type, unit, comment)
+        self[alias] = AliasEntry(alias, key, type, unit, comment)
+
+    def load(self, definition: dict[str, AliasEntry]):
+        self.update(definition)
+
+    def __getattribute__(self, alias:str) -> str:
+
+        try:
+            return super().__getattribute__(alias)
+        except AttributeError:
+            self.set(alias, alias, type, None, "")
+            return alias
+
+class AliasSystem(metaclass=Singleton):
+
+    def __new__(cls, *args, **kwargs):
+        default = NameSpace("default")
+        cls.namespaces = {"default": default}
+        default.load(DEFAULT_ALIASES)
+        return super().__new__(cls)
+    
+    def __call__(self, namespace:str) -> NameSpace:
+        if namespace not in self.namespaces:
+            self.new_namespace(namespace)
+        return self.namespaces[namespace]
+
+    def __getattribute__(self, alias: str) -> NameSpace | str:
+
+        try:
+            return super().__getattribute__(alias)
+        except AttributeError:
+
+            # namespace_or_alias_entry = self.namespaces.get(
+            #     alias, self.namespaces["default"].get(alias)
+            # )
+            # # if isinstance(namespace_or_alias_entry, NameSpace):
+            # #     return namespace_or_alias_entry
+            # # elif isinstance(namespace_or_alias_entry, AliasEntry):
+            # #     return namespace_or_alias_entry.key
+            # # else:
+            # #     self.set(alias, alias, type, None, "")
+            # #     return alias
+            # if namespace_or_alias_entry is None:  # alias not in system
+            #     self.set(alias, alias, type, None, "")
+            #     return alias
+            # else:  # alias in system
+            #     if isinstance(namespace_or_alias_entry, NameSpace):
+            #         return namespace_or_alias_entry
+            #     return namespace_or_alias_entry.key
+
+            namespace = self.namespaces.get(alias)
+            if namespace is not None:
+                return namespace
+            alias_entry = self.namespaces["default"].get(alias)
+            if alias_entry is not None:
+                return alias_entry.key
+            self.set(alias, alias, type, None, "")
+            return alias
+
+
+    def __getitem__(self, alias: str) -> AliasEntry | NameSpace:
+        try:
+            return self.namespaces[alias]
+        except KeyError:
+            return self.namespaces["default"][alias]
+
+    def new_namespace(self, name: str) -> None:
+        self.namespaces[name] = NameSpace(name)
+
+    def set(self, alias: str, key: str, type: type, unit: str, comment: str) -> None:
+        self.namespaces["default"].set(alias, key, type, unit, comment)
+
+    def list(self) -> dict[str, list[str]]:
+        """List all keys in all namespaces."""
+        return {
+            namespace.name: list(namespace.keys())
+            for namespace in self.namespaces.values()
+        }
+
+    @classmethod
+    def reset(cls):
+        """Reset the AliasSystem instance and allow to create a new instance instead of the old one."""
+        cls._instances = {}
+        return cls()
+
+    def clear(self):
+        """remove all namespaces."""
+        default = NameSpace("default")
+        self.namespaces = {"default": default}
+        default.load(DEFAULT_ALIASES)
         
-Alias = _Aliases("default", {
-            "timestep": _Alias("timestep", "_ts", int, "fs", "time step"),
-            "step": _Alias("step", "_step", int, None, "simulation step"),
-            "name": _Alias("name", "_name", str, None, "atomic name"),
-            "n_atoms": _Alias("n_atoms", "_n_atoms", int, None, "number of atoms"),
-            "n_molecules": _Alias("n_molecules", "_n_molecules", int, None, "number of molecules"),
-            "xyz": _Alias("xyz", "_xyz", np.ndarray, "angstrom", "atomic coordinates"),
-            "positions": _Alias("positions", "_xyz", np.ndarray, "angstrom", "atomic coordinates"),
-            "R": _Alias("R", "_xyz", np.ndarray, "angstrom", "atomic coordinates"),
-            "cell": _Alias("cell", "_cell", np.ndarray, "angstrom", "unit cell"),
-            "energy": _Alias("energy", "_energy", float, "meV", "energy"),
-            "forces": _Alias("forces", "_forces", np.ndarray, "eV/angstrom", "forces"),
-            "momenta": _Alias("momenta", "_momenta", np.ndarray, "eV*fs/angstrom", "momenta"),
-            "charge": _Alias("charge", "_charge", float, "C", "charge"),
-            "mass": _Alias("mass", "_mass", float, None, ""),
-            "stress": _Alias("stress", "_stress", np.ndarray, "GPa", "stress"),
-            "idx": _Alias("idx", "_idx", int, None, ""),
-            "molid": _Alias("mol", "_molid", int, None, "molecule index"),
-            "Z": _Alias("Z", "_atomic_numbers", int, None, "nuclear charge"),
-            "atype": _Alias("atype", "_atomic_types", int, None, "atomic type"),
-            "radius": _Alias("radius", "_radius", float, "angstrom", "atomic radius"),
-            "color": _Alias("color", "_color", str, None, "color"),
-            "weight": _Alias("weight", "_weight", float, None, "atomic weight"),
-            "vdw_radius": _Alias("vdw_radius", "_vdw_radius", float, "angstrom", "van der Waals radius"),
-            "idx_m": _Alias("idx_m", "_idx_m", int, None, "indices of systems"),
-            "idx_i": _Alias("idx_i", "_idx_i", int, None, "indices of center atoms"),
-            "idx_j": _Alias("idx_j", "_idx_j", int, None, "indices of neighboring atoms"),
-            "angle_i": _Alias("angle_i", "_angle_i", int, None, "indices of center atoms for angles"),
-            "angle_j": _Alias("angle_j", "_angle_j", int, None, "indices of neighboring atoms for angles"),
-            "angle_k": _Alias("angle_k", "_angle_k", int, None, "indices of neighboring atoms for angles"),
-            "bond_i": _Alias("bond_i", "_bond_i", int, None, "indices of center atoms for bonds"),
-            "bond_j": _Alias("bond_j", "_bond_j", int, None, "indices of neighboring atoms for bonds"),
-            "idx_i_lr": _Alias("idx_i_lr", "_idx_i_lr", int, None, "indices of center atoms for # long-range"),
-            "idx_j_lr": _Alias("idx_j_lr", "_idx_j_lr", int, None, "indices of neighboring atoms for # long-range"),
-            "offsets": _Alias("offsets", "_offsets", int, None, "cell offset vectors"),
-            "Rij": _Alias("Rij", "_Rij", np.ndarray, "angstrom", "vectors pointing from center atoms to neighboring atoms"),
-            "dist": _Alias("dist", "_dist", np.ndarray, "angstrom", "distances between center atoms and neighboring atoms"),
-            "pbc": _Alias("pbc", "_pbc", np.ndarray, None, "periodic boundary conditions"),
-            "dipole_moment": _Alias("dipole_moment", "_dipole_moment", np.ndarray, "e*bohr", "dipole moment"),
-            "partial_charges": _Alias("partial_charges", "_partial_charges", np.ndarray, "e", "partial charges"),
-            "polarizability": _Alias("polarizability", "_polarizability", np.ndarray, "angstrom^3", "polarizability"),
-            "element": _Alias("element", "_element", str, None, "element symbol"),
-            "atomtype": _Alias("atomtype", "_atomtype", str, None, "atom type"),
-            "bondtype": _Alias("bondtype", "_bondtype", str, None, "bond type"),
-            "angletype": _Alias("angletype", "_angletype", str, None, "angle type"),
-            "dihedraltype": _Alias("dihedraltype", "_dihedraltype", str, None, "dihedral type"),
-            "impropertype": _Alias("impropertype", "_impropertype", str, None, "improper type"),
-            "index": _Alias("index", "_index", int, None, "index"),
-        })
+Alias = AliasSystem()
