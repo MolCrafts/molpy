5fbf00875f30ad5a6be306c94042b167a51cb90d
Roy Kid <lijichen365@gmail.com>
2024-05-28 21:15:32 +0200

    [core-nblist-test] not support self-interaction

--- tests/test_core/test_nblist.py
+++ tests/test_core/test_nblist.py
@@ -41,13 +41,20 @@ class TestNaiveNblist:
 
     @pytest.mark.parametrize(
         "cutoff, self_interaction",
-        [(rc, self_interaction) for rc in [7] for self_interaction in [False, True]],
+        [(rc, self_interaction) for rc in [1, 3, 5, 7] for self_interaction in [False, True]],
     )
     def test_orth_space(self, ase_orth_tests, cutoff, self_interaction):
         is_exclude_ii = not self_interaction
         is_exclude_ji = False
 
+
         for atoms in ase_orth_tests:
+
+            # dont support "self-interaction":
+            # which means two more same pair idx in the mapping
+            if np.any(cutoff > 0.5 * np.min(atoms.cell.array)):
+                continue
+
             space = mp.Free()
             nblist = NaiveNbList()
 
@@ -62,11 +69,10 @@ class TestNaiveNblist:
             idx_i, idx_j, idx_S, expected_dist = neighbor_list(
                 "ijSd", atoms, cutoff, self_interaction=self_interaction
             )
-            # npt.assert_equal(
-            #     np.sort(np.unique(mapping, axis=0)),
-            #     np.sort(np.unique(np.stack([idx_i, idx_j], axis=-1), axis=0)),
-            #     err_msg=f"{np.unique(actual_dist)}"
-            # )
-            # npt.assert_equal(actual_dist, expected_dist, err_msg=f"{}")
-            expected_mapping = np.unique(np.stack([idx_i, idx_j], axis=-1), axis=0)
-            npt.assert_equal(mapping, expected_mapping)
+            npt.assert_equal(
+                np.sort(np.unique(mapping, axis=0)),
+                np.sort(np.unique(np.stack([idx_i, idx_j], axis=-1), axis=0)),
+                err_msg=f"{np.unique(actual_dist)}"
+            )
+            npt.assert_equal(actual_dist, expected_dist)
+            npt.assert_equal(mapping, np.stack([idx_i, idx_j], axis=-1), axis=0)
