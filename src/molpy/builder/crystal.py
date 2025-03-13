19997dc277b265a6c28d6abf44bb6876ddafaa48
Roy Kid <lijichen365@gmail.com>
2025-03-13 16:42:43 +0000

    update

--- src/molpy/builder/crystal.py
+++ src/molpy/builder/crystal.py
@@ -1,47 +1,98 @@
 import numpy as np
+from abc import ABC, abstractmethod
 from molpy.core import Region, Struct
 
-class FCC:
 
-    def __init__(self, region: Region, lattice_constant):
-        self.region = region
-        self.lattice_constant = lattice_constant
+class Lattice:
 
-    def create_lattice(self)->np.ndarray:
+    def __init__(self, sites: np.ndarray, cell: np.ndarray):
+        self.sites = sites
+        self.cell = cell
 
-        xlo, xhi = self.region.xlo, self.region.xhi
-        ylo, yhi = self.region.ylo, self.region.yhi
-        zlo, zhi = self.region.zlo, self.region.zhi
+    def repeat(self, *shape):
+        """
+        repeat sites in different directions
+        """
+        assert len(shape) == 3
 
-        dx = xhi - xlo
-        dy = yhi - ylo
-        dz = zhi - zlo
-        nx = int(dx // self.lattice_constant)
-        ny = int(dy // self.lattice_constant)
-        nz = int(dz // self.lattice_constant)
+        for x, vec in zip(shape, self.cell):
+            if x != 1 and not vec.any():
+                raise ValueError('Cannot repeat along undefined lattice '
+                                'vector')
 
-        basis = np.array([
-            [0.0, 0.0, 0.0],
-            [0.5, 0.5, 0.0],
-            [0.5, 0.0, 0.5],
-            [0.0, 0.5, 0.5]
-        ])
+        M = np.prod(shape)
+        n = len(self)
 
-        grid = np.array(np.meshgrid(np.arange(nx), np.arange(ny), np.arange(nz))).T.reshape(-1, 3)
-        lattice_points = grid[:, None, :] + basis[None, :, :]
-        lattice_points = lattice_points.reshape(-1, 3) * self.lattice_constant
-        lattice_points[:, 0] += xlo
-        lattice_points[:, 1] += ylo
-        lattice_points[:, 2] += zlo
-        return lattice_points[self.region.isin(lattice_points)]
-    
-    def fill(self, struct: Struct) -> Struct:
-        _struct = Struct()
-        _struct_template = struct()
-        _struct_template.move_to_origin()
-        lattice = self.create_lattice()
-        for xyz in lattice:
-            new_struct = _struct_template()
-            new_struct.move([xyz])
-            struct.add_struct_(new_struct)
-        return _struct
+        positions = np.empty((n * M, 3))
+        i0 = 0
+        for m0 in range(shape[0]):
+            for m1 in range(shape[1]):
+                for m2 in range(shape[2]):
+                    i1 = i0 + n
+                    positions[i0:i1] += np.dot((m0, m1, m2), self.cell)
+                    i0 = i1
+
+        self.cell = np.array([shape[c] * self.cell[c] for c in range(3)])
+
+    def fill(self, struct: Struct) -> Struct: ...
+
+
+class BaseBuilder(ABC):
+
+    @abstractmethod
+    def create_sites(self) -> np.ndarray: ...
+
+    @abstractmethod
+    def fill(self, struct: Struct) -> Struct: ...
+
+
+class CrystalBuilder(BaseBuilder):
+
+    def __init__(
+        self,
+        a: float = None,
+        b: float = None,
+        c: float = None,
+        *,
+        alpha: float = None,
+        covera: float = None,
+        u: float = None,
+        orthorhombic: bool = False,
+        cubic: bool = False,
+        basis=None,
+    ):
+
+        self.a = a
+        self.b = b
+        self.c = c
+        self.alpha = alpha
+        self.covera = covera
+        self.u = u
+        self.orthorhombic = orthorhombic
+        self.cubic = cubic
+        self.basis = basis
+
+    def create_sites(self, region):
+        ...
+
+
+class FCC(CrystalBuilder):
+
+    def __init__(
+        self,
+        a: float,
+        *,
+        orthorhombic: bool = False,
+        cubic: bool = False,
+    ):
+        b = 0.5*a
+        super().__init__(a=a, b=b, orthorhombic=orthorhombic, cubic=cubic)
+        self.cell = ((0, b, b), (b, 0, b), (b, b, 0))
+        self.basis = ((0, 0, 0), (0.5, 0.5, 0), (0.5, 0, 0.5), (0, 0.5, 0.5))
+
+    def create_lattice(self):
+        asites = Lattice(
+            self.basis * self.a,
+            self.cell
+        )
+        return asites
--- src/molpy/core/region.py
+++ src/molpy/core/region.py
@@ -2,24 +2,25 @@ import numpy as np
 import numpy.typing as npt
 from abc import abstractmethod, ABC
 
+
 class Region(ABC):
 
     def __init__(self, name):
         self.name = name
 
     @abstractmethod
-    def isin(self, xyz)->bool:
+    def isin(self, xyz) -> bool:
         raise NotImplementedError
-    
+
     @abstractmethod
-    def volumn(self)->float:
+    def volumn(self) -> float:
         raise NotImplementedError
-    
+
     @property
     @abstractmethod
     def xlo(self):
         raise NotImplementedError
-    
+
     @property
     @abstractmethod
     def xhi(self):
@@ -29,17 +30,17 @@ class Region(ABC):
     @abstractmethod
     def ylo(self):
         raise NotImplementedError
-    
+
     @property
     @abstractmethod
     def yhi(self):
         raise NotImplementedError
-    
+
     @property
     @abstractmethod
     def zlo(self):
         raise NotImplementedError
-    
+
     @property
     @abstractmethod
     def zhi(self):
@@ -49,59 +50,70 @@ class Region(ABC):
 class PeriodicBoundary(ABC):
 
     @abstractmethod
-    def wrap(self, xyz)->np.ndarray:
+    def wrap(self, xyz) -> np.ndarray:
         raise NotImplementedError
-    
+
+
 class Cube(Region):
 
-    def __init__(self, origin: npt.ArrayLike, side: int|float, name="Cube"):
+    def __init__(
+        self,
+        side: int | float,
+        origin: npt.ArrayLike = np.array([0, 0, 0]),
+        name="Cube",
+    ):
         super().__init__(name)
         self.origin = np.array(origin)
         self.side = side
 
     def isin(self, xyz):
-        return np.logical_and(np.all(self.origin <= xyz, axis=1), np.all(xyz <= self.origin + self.side, axis=1))
-    
+        return np.logical_and(
+            np.all(self.origin <= xyz, axis=1),
+            np.all(xyz <= self.origin + self.side, axis=1),
+        )
+
     def volumn(self):
         return self.side**3
-    
+
     @property
     def xlo(self):
         return self.origin[0]
-    
+
     @property
     def xhi(self):
         return self.origin[0] + self.side
-    
+
     @property
     def ylo(self):
         return self.origin[1]
-    
+
     @property
     def yhi(self):
         return self.origin[1] + self.side
-    
+
     @property
     def zlo(self):
         return self.origin[2]
-    
+
     @property
     def zhi(self):
         return self.origin[2] + self.side
-    
+
+
 class Sphere(Region):
 
-    def __init__(self, origin: npt.ArrayLike, radius: int|float, name="Sphere"):
+    def __init__(self, origin: npt.ArrayLike, radius: int | float, name="Sphere"):
         super().__init__(name)
         self.center = np.array(origin)
         self.radius = radius
 
     def isin(self, xyz):
         return np.linalg.norm(xyz - self.center) <= self.radius
-    
+
     def volumn(self):
-        return 4/3*np.pi*self.radius**3
-    
+        return 4 / 3 * np.pi * self.radius**3
+
+
 class Cuboid(Region):
 
     def __init__(self, origin: npt.ArrayLike, lengths: npt.ArrayLike, name="Cuboid"):
@@ -109,15 +121,15 @@ class Cuboid(Region):
         self.origin = np.array(origin)
         self.upper = self.origin + lengths
         self.lengths = lengths
-        
+
     def isin(self, xyz):
         return np.all(self.origin <= xyz) and np.all(xyz <= self.upper)
-    
+
     def volumn(self):
         return np.prod(self.lengths)
-    
+
     def constrain(self, xyz):
-        
+
         upper = xyz - self.upper
         lower = self.origin - xyz
         tmp = np.max(np.concat([upper, lower]))
